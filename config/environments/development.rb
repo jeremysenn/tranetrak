1f54de59e4d99ff33353365f54af4d4cb93849c1
Jeremy Senn <jeremysenn@Jeremy-Senns-MacBook-Pro.local>
2013-11-17 08:52:41 -0800

    smtp email error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Bcomp::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "tranact.com",
+    domain: "jeremysenn.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
